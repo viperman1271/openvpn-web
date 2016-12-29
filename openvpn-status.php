4bb057c831b5381dbe4b3b917d6bccbf56343cca
viperman1271 <mfilion@mikefilion.com>
2016-12-29 16:54:34 -0500

    Style update
    Adding a new header to the openvpn-status.php and favicon.ico to the
    repo

--- openvpn-status.php
+++ openvpn-status.php
@@ -51,40 +51,40 @@ function sizeformat($bytesize){
     return("$newsize $units[$i]");
 }
 $stats = parseLog("/etc/openvpn/status.log");
-echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<body>
-    <center>';
 echo '
-        <h3>Active Users</h3> 
-        <table>
-            <tr style="font-weight: bold;" bgcolor="#888888">
-                <td>Common Name</td>
-                <td>Real Address</td>
-                <td>Virtual Address</td>
-                <td>Logged In At</td>
-                <td>Bytes Sent</td>
-                <td>Bytes Received</td>
-                <td>Last Activity</td>
-            </tr>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+    <body>
+        <center>';
+echo '
+            <h3>Active Users</h3> 
+            <table>
+                <tr style="font-weight: bold;" bgcolor="#888888">
+                    <td>Common Name</td>
+                    <td>Real Address</td>
+                    <td>Virtual Address</td>
+                    <td>Logged In At</td>
+                    <td>Bytes Sent</td>
+                    <td>Bytes Received</td>
+                    <td>Last Activity</td>
+                </tr>
 ';
 foreach($stats['users'] as $user)
 {  
     echo '
-            <tr bgcolor="#eeeeee">
-                <td>'.$user['CommonName'].'</td>
-                <td>'.$user['RealAddress'].'</td>
-                <td>'.$user['VirtualAddress'].'</td>
-                <td>'.$user['Since'].'</td>
-                <td>'.$user['BytesReceived'].'</td>
-                <td>'.$user['BytesSent'].'</td>
-                <td>'.$user['LastRef'].'</td>
-            </tr>';
+                <tr bgcolor="#eeeeee">
+                    <td>'.$user['CommonName'].'</td>
+                    <td>'.$user['RealAddress'].'</td>
+                    <td>'.$user['VirtualAddress'].'</td>
+                    <td>'.$user['Since'].'</td>
+                    <td>'.$user['BytesReceived'].'</td>
+                    <td>'.$user['BytesSent'].'</td>
+                    <td>'.$user['LastRef'].'</td>
+                </tr>';
 }
 echo '
-        </table>
-        <br>
-    </center>
-</body>
+            </table>
+        </center>
+    </body>
 </html>';
 ?>
 
