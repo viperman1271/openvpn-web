f6cd361938ff300fb336bc512e134df7470fbaaa
viperman1271 <mfilion@mikefilion.com>
2016-12-30 21:19:24 -0500

    cron/install files update
    Basic install created; Enured that the install function validates the
    ability to connect, select the database, and we can create the table.
    The basic cron file is now created, with some logging when there is an
    error.

--- openvpn-cron.php
+++ openvpn-cron.php
@@ -1,4 +1,4 @@
-<?
+<?php
 //////////////////////////////////////////////////////////////
 // PHP Script that will log the information from the OpenVPN
 // status log and store it into the database
@@ -7,77 +7,36 @@
 // http://jeroen.pro/2011/09/openvpn-stats-page-using-php/
 //////////////////////////////////////////////////////////////
 
-function parseLog ($log) {
-    $handle = fopen($log, "r");
-    $uid = 0;
-    while (!feof($handle)) {
-        $buffer = fgets($handle, 4096);
-        unset($match);
-        if (ereg("^Updated,(.+)", $buffer, $match)) {
-            $status['updated'] = $match[1];
-        }
-        if (preg_match("/^(.+),(\d+\.\d+\.\d+\.\d+\:\d+),(\d+),(\d+),(.+)$/", $buffer, $match)) {
-            if ($match[1] <> "Common Name") {
-                $cn = $match[1];
- 
-                $userlookup[$match[2]] = $uid;
-                $status['users'][$uid]['CommonName'] = $match[1];
-                $status['users'][$uid]['RealAddress'] = $match[2];
-                $status['users'][$uid]['BytesReceived'] = $match[3];
-                $status['users'][$uid]['BytesSent'] = $match[4];
-                $status['users'][$uid]['Since'] = $match[5];
-                $uid++;
-            }
-        }
-        if (preg_match("/^(\d+\.\d+\.\d+\.\d+),(.+),(\d+\.\d+\.\d+\.\d+\:\d+),(.+)$/", $buffer, $match)) {
-            if ($match[1] <> "Virtual Address") {
-                $address = $match[3];
-                $uid = $userlookup[$address];
-                $status['users'][$uid]['VirtualAddress'] = $match[1];
-                $status['users'][$uid]['LastRef'] = $match[4];
-            }
-        }
-    }
-    fclose($handle);
-    return($status);
-}
+require 'config/mysql.php';
+require 'config/openvpn.php';
 
-//////////////////////////////////////////////////////////////
-    
-function sizeformat($bytesize){
-    $i=0;
-    while(abs($bytesize) >= 1024){
-        $bytesize=$bytesize/1024;
-        $i++;
-        if($i==4) break;
-    }
-    $units = array("Bytes","KB","MB","GB","TB");
-    $newsize=round($bytesize,2);
-    return("$newsize $units[$i]");
-}
+require 'functions/functions.php';
 
 //////////////////////////////////////////////////////////////
 
-function main() {
-    $conn = mysql_connect('localhost', 'openvpn', 'openvpn');
-    if(!$conn) {
-        echo 'Could not connect to the MySQL Database.';
+function cron() 
+{
+    $conn = mysql_connect(get_mysql_host(), get_mysql_user(), get_mysql_pass());
+    if(!$conn) 
+    {
+        logMsg('Could not connect to the MySQL Database.');
+        return;
     }
-    mysql_select_db('openvpn');
+    mysql_select_db(get_mysql_db());
 
-    $stats = parseLog("/etc/openvpn/openvpn.log");
+    $stats = parseLog(get_openvpn_status());
     foreach($stats['users'] as $user)
     {
         if($user['CommonName'] != "UNDEF")
         {
            $result = mysql_query("UPDATE stats SET updated = '".time()."', VirtualAddress = '".$user['VirtualAddress']."', BytesSent='".$user['BytesSent']."', BytesReceived='".$user['BytesReceived']."', LastRef='".$user['LastRef']."' WHERE CommonName='".$user['CommonName']."' AND Since='".$user['Since']."'") or die(mysql_error());
-           echo mysql_affected_rows();
-           if (mysql_affected_rows()==0) {
+           if (mysql_affected_rows() == 0) 
+           {
               $result = mysql_query("insert into stats (CommonName, RealAddress, BytesReceived, BytesSent, Since, VirtualAddress, LastRef) values ('".$user['CommonName']."', '".$user['RealAddress']."', '".$user['BytesReceived']."', '".$user['BytesSent']."', '".$user['Since']."', '".$user['VirtualAddress']."', '".$user['LastRef']."')");
            }
         }
     }
 }
 
-main();
+cron();
 ?>
--- openvpn-install.php
+++ openvpn-install.php
@@ -1,3 +1,6 @@
+<?php
+
+/*
 CREATE TABLE IF NOT EXISTS `stats` (
 `CommonName` text NOT NULL,
 `RealAddress` text NOT NULL,
@@ -10,3 +13,44 @@ CREATE TABLE IF NOT EXISTS `stats` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 * * * * * cd [cron location]; php cron.php
+*/
+
+function install()
+{
+    $conn = mysql_connect(get_mysql_host(), get_mysql_user(), get_mysql_pass());
+    if(!$conn) 
+    {
+        logMsg('Could not connect to the MySQL Server. Please validate your login & connection information:');
+        logMsg('    host: ' . get_mysql_host());
+        logMsg('    user: ' . get_mysql_user());
+        logMsg('    ' . mysql_error());
+        return;
+    }
+    
+    $selectDb = mysql_select_db(get_mysql_db());
+    if(!$selectDb)
+    {
+        logMsg('Could not select the MySQL Database. Please validate the login information and permissions on the database.');
+        logMsg('    db: ' + get_mysql_db());
+        logMsg('    ' . mysql_error());
+    }
+    
+    $sql = 'CREATE TABLE IF NOT EXISTS `stats` (
+            `CommonName` text NOT NULL,
+            `RealAddress` text NOT NULL,
+            `BytesReceived` text NOT NULL,
+            `BytesSent` text NOT NULL,
+            `Since` text NOT NULL,
+            `VirtualAddress` text NOT NULL,
+            `LastRef` text NOT NULL,
+            `updated` bigint(20) NOT NULL
+           ) ENGINE=MyISAM DEFAULT CHARSET=latin1;'
+    $result = mysql_query($sql);
+    if(!$result)
+    {
+        logMsg("Could not create table: ". mysql_error());
+    }
+}
+
+install();
+?>
