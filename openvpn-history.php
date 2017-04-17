f51d7a6feb9cd6c11555c88dae85ee02b6197497
viperman1271 <viperman1271@hotmail.com>
2017-04-16 21:01:19 -0400

    Formatting for history.php

--- openvpn-history.php
+++ openvpn-history.php
@@ -16,22 +16,21 @@ function history()
 {
     $stats = parseLog(get_openvpn_status());
     echo '
-    <div class="container body-content">
-        <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Active Users</h3>
-            </div>
-            <div class="box-body no-padding">
-                <table class="table table-striped">
-                    <tr style="font-weight: bold;" bgcolor="#888888">
-                        <td>Common Name</td>
-                        <td>Real Address</td>
-                        <td>Virtual Address</td>
-                        <td>Logged In At</td>
-                        <td>Received</td>
-                        <td>Sent</td>
-                        <td>Last Activity</td>
-                    </tr>
+    <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Active Users</h3>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table table-striped">
+                <tr style="font-weight: bold;" bgcolor="#888888">
+                    <td>Common Name</td>
+                    <td>Real Address</td>
+                    <td>Virtual Address</td>
+                    <td>Logged In At</td>
+                    <td>Received</td>
+                    <td>Sent</td>
+                    <td>Last Activity</td>
+                </tr>
 ';
     foreach($stats['users'] as $user)
     {  
@@ -51,87 +50,99 @@ function history()
             </div>';
     mysql_connect(get_mysql_host(), get_mysql_user(), get_mysql_pass());
     mysql_select_db(get_mysql_db());
-    echo '</div>';
-    echo '<div class="box">
-    <div class="box-header">
-        <h3 class="box-title">Bandwith Totals - All Time</h3>
+    echo '
     </div>
-    <div class="box-body no-padding">
-        <table class="table table-striped">
-            <tr style="font-weight: bold;" bgcolor="#888888">
-                <td>Common Name</td>
-                <td>Received</td>
-                <td>Sent</td>
-            </tr>
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Bandwith Totals - All Time</h3>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table table-striped">
+                <tr style="font-weight: bold;" bgcolor="#888888">
+                    <td>Common Name</td>
+                    <td>Received</td>
+                    <td>Sent</td>
+                </tr>
     ';
     $result = mysql_query("SELECT sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) 
     {
         echo '          
-            <tr bgcolor="#eeeeee">
-                <td>Total</td>
-                <td>'.sizeformat($row['totalsend']).'</td>
-                <td>'.sizeformat($row['totalreceived']).'</td>
-            </tr>';
+                <tr bgcolor="#eeeeee">
+                    <td>Total</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
     unset($result);
     $result = mysql_query("SELECT CommonName, sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats GROUP BY CommonName ORDER BY CommonName ASC") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) 
     {
-        echo '<tr bgcolor="#eeeeee">
-            <td>'.$row['CommonName'].'</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-           </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>'.$row['CommonName'].'</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
-    echo '</table></div></div>';
+    echo '
+            </table>
+        </div>
+    </div>';
 
     unset($result);
 
-    echo '<div class="box">
-    <div class="box-header">
-        <h3 class="box-title">Bandwidth Totals - Today</h3>
-    </div>
-    <div class="box-body no-padding">
-    <table class="table table-striped">
-        <tr style="font-weight: bold;" bgcolor="#888888">
-            <td>Common Name</td>
-            <td>Received</td>
-            <td>Sent</td>
-        </tr>';
+    echo '
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Bandwidth Totals - Today</h3>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table table-striped">
+                <tr style="font-weight: bold;" bgcolor="#888888">
+                    <td>Common Name</td>
+                    <td>Received</td>
+                    <td>Sent</td>
+                </tr>';
     $result = mysql_query("SELECT sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats WHERE LastRef LIKE '%".date("M")." ".date("j")."%".date("Y")."%'") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) 
     {
-        echo '<tr bgcolor="#eeeeee">
-            <td>Total</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-        </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>Total</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
     unset($result);
     $result = mysql_query("SELECT CommonName, sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats WHERE LastRef LIKE '%".date("M")." ".date("j")."%".date("Y")."%' GROUP BY CommonName ") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) 
     {
-        echo '<tr bgcolor="#eeeeee">
-            <td>'.$row['CommonName'].'</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-        </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>'.$row['CommonName'].'</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
-    echo '</table></div></div>';
+    echo '
+            </table>
+        </div>
+    </div>';
 
     unset($result);
-    echo '<div class="box">
-    <div class="box-header">
-        <h3 class="box-title">Bandwidth Totals - Last 7 Days</h3>
-    </div>
-    <div class="box-body no-padding">
-    <table class="table table-striped">
-        <tr style="font-weight: bold;" bgcolor="#888888">
-            <td>Common Name</td>
-            <td>Bytes Received</td>
-            <td>Bytes Sent</td>
-        </tr>';
+    echo '
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Bandwidth Totals - Last 7 Days</h3>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table table-striped">
+                <tr style="font-weight: bold;" bgcolor="#888888">
+                    <td>Common Name</td>
+                    <td>Bytes Received</td>
+                    <td>Bytes Sent</td>
+                </tr>';
     $like = "LastRef LIKE '%".date("M")." ".date("j")."%".date("Y")."%'";
     for ($i=0; $i<7; $i++)
     {
@@ -140,91 +151,106 @@ function history()
     $result = mysql_query("SELECT sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats WHERE ".$like."") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) 
     {
-        echo '<tr bgcolor="#eeeeee">
-            <td>Total</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-        </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>Total</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
     unset($result);
     $result = mysql_query("SELECT CommonName, sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats WHERE ".$like." GROUP BY CommonName ") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) 
     {
-        echo '<tr bgcolor="#eeeeee">
-            <td>'.$row['CommonName'].'</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-        </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>'.$row['CommonName'].'</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
-    echo '</table></div></div>';
+    echo '
+            </table>
+        </div>
+    </div>';
 
     unset($result);
 
-    echo '<div class="box">
-    <div class="box-header">
-        <h3 class="box-title">Bandwidth Totals - This Month</h3>
-    </div>
-    <div class="box-body no-padding">
-    <table class="table table-striped">
-        <tr style="font-weight: bold;" bgcolor="#888888">
-            <td>Common Name</td>
-            <td>Bytes Received</td>
-            <td>Bytes Sent</td>
-        </tr>';
+    echo '
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Bandwidth Totals - This Month</h3>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table table-striped">
+                <tr style="font-weight: bold;" bgcolor="#888888">
+                    <td>Common Name</td>
+                    <td>Bytes Received</td>
+                    <td>Bytes Sent</td>
+                </tr>';
     $result = mysql_query("SELECT sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats WHERE LastRef LIKE '%".date("M")."%".date("Y")."%'") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) 
     {
-        echo '<tr bgcolor="#eeeeee">
-            <td>Total</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-        </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>Total</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
     unset($result);
     $result = mysql_query("SELECT CommonName, sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats WHERE LastRef LIKE '%".date("M")."%".date("Y")."%' GROUP BY CommonName ") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) 
     {
-        echo '<tr bgcolor="#eeeeee">
-            <td>'.$row['CommonName'].'</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-        </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>'.$row['CommonName'].'</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
-    echo '</table></div></div>';
+    echo '
+            </table>
+        </div>
+    </div>';
 
     unset($result);
 
-    echo '<h3>Bandwidth Totals - This Year</h3>
+    echo '
     <div class="box">
-    <div class="box-header">
-        <h3 class="box-title">Bandwidth Totals - This Year</h3>
-    </div>
-    <div class="box-body no-padding">
-    <table class="table table-striped">
-        <tr style="font-weight: bold;" bgcolor="#888888">
-            <td>Common Name</td>
-            <td>Bytes Received</td>
-            <td>Bytes Sent</td>
-        </tr>';
+        <div class="box-header">
+            <h3 class="box-title">Bandwidth Totals - This Year</h3>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table table-striped">
+                <tr style="font-weight: bold;" bgcolor="#888888">
+                    <td>Common Name</td>
+                    <td>Bytes Received</td>
+                    <td>Bytes Sent</td>
+                </tr>';
     $result = mysql_query("SELECT sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats WHERE LastRef LIKE '%".date("Y")."%'") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) {
-        echo '<tr bgcolor="#eeeeee">
-            <td>Total</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-        </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>Total</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
     unset($result);
     $result = mysql_query("SELECT CommonName, sum(BytesSent) as 'totalsend', sum(BytesReceived) as 'totalreceived' FROM stats WHERE LastRef LIKE '%".date("Y")."%' GROUP BY CommonName ") or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) {
-        echo '<tr bgcolor="#eeeeee">
-            <td>'.$row['CommonName'].'</td>
-            <td>'.sizeformat($row['totalsend']).'</td>
-            <td>'.sizeformat($row['totalreceived']).'</td>
-        </tr>';
+        echo '
+                <tr bgcolor="#eeeeee">
+                    <td>'.$row['CommonName'].'</td>
+                    <td>'.sizeformat($row['totalsend']).'</td>
+                    <td>'.sizeformat($row['totalreceived']).'</td>
+                </tr>';
     }
-    echo '</table></div></div>';
-    echo '</div>';
+    echo '
+            </table>
+        </div>
+    </div>';
 }
 
 require 'include/page_vars.php';
