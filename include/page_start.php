1a3b3089a0641b18d69aada9b0c333ba38e528fd
Michael Filion <viperman1271@hotmail.com>
2017-04-17 19:45:42 -0400

    Implemented right hover menu

--- include/page_start.php
+++ include/page_start.php
@@ -106,6 +106,34 @@ desired effect
         <span class="sr-only">Toggle navigation</span>
       </a>
       <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- User Account Menu -->
+          <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <!-- The user image in the navbar-->
+              <img src="images/ovpnicon.png" class="user-image" alt="User Image">
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs"><b>OpenVPN</b> Gateway</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- The user image in the menu -->
+              <li class="user-header">
+                <img src="images/ovpnicon.png" class="img-circle" alt="User Image">
+                <p>
+                  Open Source Web UI
+                  <small>for managing OpenVPN</small>
+                </p>
+              </li>
+              <!-- Menu Body -->
+              <li class="user-body">
+                <a href="https://github.com/viperman1271/openvpn-web" target="_blank"><center>Available on GitHub</center></a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
   <!-- Left side column. contains the logo and sidebar -->
