2aea41674a7cdc8355c59655668e70703a74826d
viperman1271 <viperman1271@hotmail.com>
2017-04-16 23:37:56 -0400

    Revert "Reverting some stuff"
    This reverts commit 5692d7b70be4a306a3c1ffcd0f41bc6de437b148.

---
+++ install.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+CREATE TABLE IF NOT EXISTS `stats` (
+`CommonName` text NOT NULL,
+`RealAddress` text NOT NULL,
+`BytesReceived` text NOT NULL,
+`BytesSent` text NOT NULL,
+`Since` text NOT NULL,
+`VirtualAddress` text NOT NULL,
+`LastRef` text NOT NULL,
+`updated` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+* * * * * cd [cron location]; php cron.php
+*/
+
+require 'config/mysql.php';
+require 'config/openvpn.php';
+
+require 'functions/functions.php';
+
+function install()
+{
+    $conn = mysql_connect(get_mysql_host(), get_mysql_user(), get_mysql_pass());
+    if(!$conn) 
+    {
+        logMsg('Could not connect to the MySQL Server. Please validate your login & connection information:');
+        logMsg('    host: ' . get_mysql_host());
+        logMsg('    user: ' . get_mysql_user());
+        logMsg('    ' . mysql_error());
+        return;
+    }
+    
+    $selectDb = mysql_select_db(get_mysql_db());
+    if(!$selectDb)
+    {
+        logMsg('Could not select the MySQL Database. Please validate the login information and permissions on the database.');
+        logMsg('    db: ' + get_mysql_db());
+        logMsg('    ' . mysql_error());
+        return;
+    }
+    
+    $sql = 'CREATE TABLE IF NOT EXISTS `stats` (
+            `CommonName` text NOT NULL,
+            `RealAddress` text NOT NULL,
+            `BytesReceived` text NOT NULL,
+            `BytesSent` text NOT NULL,
+            `Since` text NOT NULL,
+            `VirtualAddress` text NOT NULL,
+            `LastRef` text NOT NULL,
+            `updated` bigint(20) NOT NULL
+           ) ENGINE=MyISAM DEFAULT CHARSET=latin1;';
+    $result = mysql_query($sql);
+    if(!$result)
+    {
+        logMsg("Could not create table: ". mysql_error());
+        return;
+    }
+    
+    $tmpFile = '/tmp/crontab.txt';
+    $output = shell_exec('crontab -l');
+    file_put_contents($tmpFile, $output);
+    $newCron = '* * * * * cd ' . dirname(__FILE__) . '; php ' .  dirname(__FILE__) . '/openvpn-cron.php';
+    if( strpos($output,$newCron) !== false)
+    {
+        //Do nothing
+    }
+    else
+    {
+        file_put_contents($tmpFile, $output.$newCron.PHP_EOL);
+        echo exec('crontab '.$tmpFile);
+    }
+    
+    $errors = 0;
+    //Validate that the time zone settings are correct
+    if(date_default_timezone_get()!=ini_get('date.timezone'))
+    {
+       $errors += 1;
+       echo "ensure that the 'date.timezone' value in the php.ini has been set correctly".PHP_EOL;
+    }
+
+echo'
+    <section class="content-header">
+      <h1>Install</h1>
+      <ol class="breadcrumb">
+        <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li>Admin</li>
+        <li class="active">Install</li>
+      </ol>
+    </section>
+    <br/>
+    <div class="box">
+        <center>
+';
+    if($errors==0)
+    {
+        echo '            <h1>Install Succeeded</h1>';
+    }
+    else
+    {
+        echo '            Install had some warnings';
+    }
+echo '
+        </center>
+        <br/>
+    </div>
+';
+}
+
+require 'include/page_vars.php';
+
+$page_install = TRUE;
+
+require 'include/page_start.php';
+require 'include/page_end.php';
+
+echo $page_start;
+
+install();
+
+echo $page_end;
+?>
---
+++ status.php
@@ -0,0 +1,75 @@
+<?php
+//////////////////////////////////////////////////////////////
+// PHP script that will parse the openvpn status file
+// and display the result in HTML format
+//
+// Modified version from: 
+// http://jeroen.pro/2011/09/openvpn-stats-page-using-php/
+//////////////////////////////////////////////////////////////
+
+require 'config/mysql.php';
+require 'config/openvpn.php';
+
+require 'functions/functions.php';
+
+require 'include/page_vars.php';
+
+$page_status = TRUE;
+
+require 'include/page_start.php';
+require 'include/page_end.php';
+
+echo $page_start;
+
+//////////////////////////////////////////////////////////////
+
+$stats = parseLog("/etc/openvpn/status.log");
+
+echo'
+        <section class="content-header">
+            <h1>Status</h1>
+            <ol class="breadcrumb">
+                <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active"><i class="fa fa-files-o"></i> Status</li>
+            </ol>
+        </section>
+        <br/>
+        <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Active Users</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body no-padding">
+                <table class="table table-striped">
+                    <tr>
+                        <th>Common Name</th>
+                        <th>Real Address</th>
+                        <th>Virtual Address</th>
+                        <th>Logged In At</th>
+                        <th>Received</th>
+                        <th>Sent</th>
+                        <th>Last Activity</th>
+                    </tr>
+';
+
+foreach($stats['users'] as $user)
+{  
+    echo '
+                    <tr>
+                        <td>'.$user['CommonName'].'</td>
+                        <td>'.$user['RealAddress'].'</td>
+                        <td>'.$user['VirtualAddress'].'</td>
+                        <td>'.$user['Since'].'</td>
+                        <td>'.$user['DataSent'].'</td>
+                        <td>'.$user['DataReceived'].'</td>
+                        <td>'.$user['LastRef'].'</td>
+                    </tr>';
+}
+echo '
+                </table>
+            </div>
+            <center>Live status Last Updated: <b>'.$stats['updated'].'</b>
+        </div>';
+            
+echo $page_end;
+?>
